d38850b3fa6f27ee4e16845bba0167bfe1709343
Aarush <aarush.g512@gmail.com>
2025-11-01 15:11:29 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+# This prints a random joke 
+print("printing jokes....")
+jokes = pyjokes.get_joke()
+print(jokes)
+
+# thnaks that was my program
+# another line 
+# yet another line
